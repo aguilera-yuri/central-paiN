d370a850e13384ffc06382515600d054bef3fccd
barney901 <yuri.proenca@sptech.school>
2026-05-16 15:03:32 -0300

    checkpoint front-end

---
+++ jogadores.js
@@ -0,0 +1,336 @@
+let jogadoresLolPrincipal = [
+    {
+        nome: "Robo",
+        foto: "assets/players/robo.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/top.png"
+    },
+
+    {
+        nome: "CarioK",
+        foto: "assets/players/cariok.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/jungle.png"
+    },
+
+    {
+        nome: "Keine",
+        foto: "assets/players/keine.png",
+        bandeira: "assets/iconplayers/korea.png",
+        icone: "assets/iconplayers/mid.png"
+    },
+
+    {
+        nome: "Trigger",
+        foto: "assets/players/trigger.png",
+        bandeira: "assets/iconplayers/korea.png",
+        icone: "assets/iconplayers/bot.png"
+    },
+
+    {
+        nome: "Kuri",
+        foto: "assets/players/kuri.png",
+        bandeira: "assets/iconplayers/korea.png",
+        icone: "assets/iconplayers/sup.png"
+    }
+];
+
+let jogadoresLolAcademy = [
+    {
+        nome: "Zoen",
+        foto: "assets/players/zoen.png",
+        bandeira: "assets/iconplayers/argentina.png",
+        icone: "assets/iconplayers/top.png"
+    },
+    {
+        nome: "Samkz",
+        foto: "assets/players/samkz.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/jungle.png"
+    },
+    {
+        nome: "Namiru",
+        foto: "assets/players/namiru.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/mid.png"
+    },
+    {
+        nome: "Marvin",
+        foto: "assets/players/marvin.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/bot.png"
+    },
+    {
+        nome: "Gatovisck",
+        foto: "assets/players/gatovisck.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: "assets/iconplayers/sup.png"
+    }
+];
+
+let jogadoresCsPrincipal = [
+    {
+        nome: "v$m",
+        foto: "assets/players/vsm.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "nqz",
+        foto: "assets/players/nqz.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "biguzera",
+        foto: "assets/players/biguzera.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "snow",
+        foto: "assets/players/snow.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "piriajr",
+        foto: "assets/players/piriajr.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    }
+];
+
+let jogadoresCsAcademy = [
+    {
+        nome: "Tatu",
+        foto: "assets/players/tatu.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "k9izer",
+        foto: "assets/players/k9izer.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "Markz",
+        foto: "assets/players/markz.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "tsug",
+        foto: "assets/players/tsug.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+    {
+        nome: "rkzinho",
+        foto: "assets/players/rkzinho.png",
+        bandeira: "assets/iconplayers/brasil.png",
+        icone: ""
+    },
+];
+
+let jogadoresKings = [
+    {
+        nome: "renato",
+        foto: "assets/players/renato.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "chorume",
+        foto: "assets/players/chorume.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "camargo",
+        foto: "assets/players/camargo.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "kayky 2k",
+        foto: "assets/players/2k.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "guilherme",
+        foto: "assets/players/gui_nasci.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "bolt",
+        foto: "assets/players/bolt.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "profeta",
+        foto: "assets/players/profeta.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "gigante",
+        foto: "assets/players/gigante.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "alemao",
+        foto: "assets/players/alemao.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "juvenal",
+        foto: "assets/players/juvenal.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "VB",
+        foto: "assets/players/VB.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "henry japa",
+        foto: "assets/players/japa.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "william",
+        foto: "assets/players/williamCosta.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "luisinho",
+        foto: "assets/players/luisinho.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "davi",
+        foto: "assets/players/davi.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "douglinhas",
+        foto: "assets/players/douglinhas.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "christian",
+        foto: "assets/players/chris.png",
+        bandeira: "assets/iconplayers/brasil.png"
+    }
+];
+let jogadoresGta = [
+    {
+        nome: "GTA 1",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "GTA 2",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "GTA 3",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "GTA 4",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "GTA 5",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    },
+    {
+        nome: "GTA 6",
+        foto: "",
+        bandeira: "assets/iconplayers/brasil.png"
+    }
+];
+
+let estiloAtivo = "padding:14px 32px; border-radius:50px; border:1.5px solid #e8192c; background:#e8192c; color:#fff; font-size:13px; font-weight:700";
+let estiloInativo = "padding:14px 32px; border-radius:50px; border:1.5px solid #3a3a3a; background:transparent; color:#fff; font-size:13px; font-weight:700";
+
+const paineis = ['lol_principal', 'lol_academy', 'cs_principal', 'cs_academy', 'kings', 'gta'];
+
+function criarCard(jogador) {
+    let img = '<img src="' + jogador.foto + '">';
+    let icone = '<img src="' + jogador.icone + '">';
+    let bandeira = '<img src="' + jogador.bandeira + '">';
+
+    let card = '<div class="jogador-card">';
+    card += '<div class="foto-jogador">' + img + '</div>';
+    card += '<div class="infos-jogador">';
+    card += '<div class="bandeira">' + bandeira + '</div>';
+    card += '<span class="nome-jogador">' + jogador.nome + '</span>';
+    card += '<span class="icon-role">' + icone + '</span>';
+    card += '</div></div>';
+    return card;
+}
+
+function preencherGrid(elemento, vetor) {
+    let card = "";
+    for (let i = 0; i < vetor.length; i++) {
+        card += criarCard(vetor[i]);
+    }
+    elemento.innerHTML = card;
+}
+
+preencherGrid(grid_lol_principal, jogadoresLolPrincipal);
+preencherGrid(grid_lol_academy, jogadoresLolAcademy);
+preencherGrid(grid_cs_principal, jogadoresCsPrincipal);
+preencherGrid(grid_cs_academy, jogadoresCsAcademy);
+preencherGrid(grid_kings, jogadoresKings);
+preencherGrid(grid_gta, jogadoresGta);
+
+function trocarModalidade(ativa) {
+
+    painel_lol_principal.style.display = 'none';
+    btn_lol_principal.style.cssText = estiloInativo;
+
+    painel_lol_academy.style.display = 'none';
+    btn_lol_academy.style.cssText = estiloInativo;
+
+    painel_cs_principal.style.display = 'none';
+    btn_cs_principal.style.cssText = estiloInativo;
+
+    painel_cs_academy.style.display = 'none';
+    btn_cs_academy.style.cssText = estiloInativo;
+
+    painel_kings.style.display = 'none';
+    btn_kings.style.cssText = estiloInativo;
+
+    painel_gta.style.display = 'none';
+    btn_gta.style.cssText = estiloInativo;
+
+    if (ativa == 'lol_principal') {
+        painel_lol_principal.style.display = 'block';
+        btn_lol_principal.style.cssText = estiloAtivo;
+    }
+    else if (ativa == 'lol_academy') {
+        painel_lol_academy.style.display = 'block';
+        btn_lol_academy.style.cssText = estiloAtivo;
+    }
+    else if (ativa == 'cs_principal') {
+        painel_cs_principal.style.display = 'block';
+        btn_cs_principal.style.cssText = estiloAtivo;
+    }
+    else if (ativa == 'cs_academy') {
+        painel_cs_academy.style.display = 'block';
+        btn_cs_academy.style.cssText = estiloAtivo;
+    }
+    else if (ativa == 'kings') {
+        painel_kings.style.display = 'block';
+        btn_kings.style.cssText = estiloAtivo;
+    }
+    else if (ativa == 'gta') {
+        painel_gta.style.display = 'block';
+        btn_gta.style.cssText = estiloAtivo;
+    }
+}
